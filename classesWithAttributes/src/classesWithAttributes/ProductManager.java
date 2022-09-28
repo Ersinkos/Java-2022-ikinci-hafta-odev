d3cbe55c6d0b0102a4aaa1389fb80d61812aff5f
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2022-09-28 20:12:09 +0300

    Attribute,field,encapsulation,getter and setter,constructor

---
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -0,0 +1,11 @@
+package classesWithAttributes;
+
+public class ProductManager {
+
+	public void Add(Product product) {
+		// JDBS
+		System.out.println("Ürün eklendi: " + product.getName());
+
+	}
+
+}
